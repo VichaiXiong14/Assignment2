a0c79cb742ad5f495e7fb0720be77de11760c048
Vichai Xiong <xiongv16@uwosh.edu>
2020-11-18 14:24:16 -0600

    Added package.json. PORT changes to web server, procfile

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
 var q = url.parse(req.url, true);
@@ -26,6 +27,7 @@ res.write(data);
 return res.end();
 });
 
-}).listen(8080);
-console.log("Server Listening on Port 8080...");
+}).listen(PORT);
+
+console.log("Server Listening on assigned PORT...");
 
